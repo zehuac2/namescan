78806b647688866b322851370cfa16702a37b87a
Zehua Chen <31496190+zehuac2@users.noreply.github.com>
2025-06-29 23:25:10 -0700

    [feat] Implement CustomStringConvertible for FilenameScannerResult

--- Sources/FilenamesScan/Scan/FilenameScannerResult.swift
+++ Sources/FilenamesScan/Scan/FilenameScannerResult.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum FilenameScannerResult: Equatable {
+public enum FilenameScannerResult: Equatable, CustomStringConvertible {
   public enum OS {
     case windows
     case linux
@@ -15,4 +15,13 @@ public enum FilenameScannerResult: Equatable {
 
   case ok(url: URL)
   case invalid(url: URL, character: Character, os: OS)
+
+  public var description: String {
+    switch self {
+    case .ok(let url):
+      return "OK: \(url)"
+    case .invalid(let url, let character, let os):
+      return "Invalid: \(url), Character: \(character), OS: \(os)"
+    }
+  }
 }
