a3c6244dd24854c5ccf70876743ec8c506d8030f
Zehua Chen <31496190+zehuac2@users.noreply.github.com>
2025-06-29 22:57:20 -0700

    [chore] Migrate to new test framework and setup build workflow.

--- Tests/FilenamesScanTests/FilenameScannerTests.swift
+++ Tests/FilenamesScanTests/FilenameScannerTests.swift
@@ -4,27 +4,25 @@
 //
 //  Created by Zehua Chen on 5/20/23.
 //
-
-import XCTest
+import Foundation
+import Testing
 
 @testable import FilenamesScan
 
-final class FilenameScannerTests: XCTestCase {
-  func testWindows() throws {
-    testPath("\\test.txt", invalidCharacter: "\\", os: .windows)
-    testPath("test?.txt", invalidCharacter: "?", os: .windows)
-    testPath("<test.txt", invalidCharacter: "<", os: .windows)
-    testPath("test>.txt", invalidCharacter: ">", os: .windows)
-    testPath("test:.txt", invalidCharacter: ":", os: .windows)
-    testPath("test|", invalidCharacter: "|", os: .windows)
-    testPath("te\"st.txt", invalidCharacter: "\"", os: .windows)
-  }
+func testPath(_ path: String, invalidCharacter: Character, os: FilenameScannerResult.OS) {
+  let url = URL(filePath: path)
+  let filenameScanner = FilenameScanner()
+  let results = filenameScanner.scan(url)
 
-  func testPath(_ path: String, invalidCharacter: Character, os: FilenameScannerResult.OS) {
-    let url = URL(filePath: path)
-    let filenameScanner = FilenameScanner()
-    let results = filenameScanner.scan(url)
+  #expect(results == [.invalid(url: url, character: invalidCharacter, os: os)])
+}
 
-    XCTAssertEqual(results, [.invalid(url: url, character: invalidCharacter, os: .windows)])
-  }
+@Test func windows() {
+  testPath("\\test.txt", invalidCharacter: "\\", os: .windows)
+  testPath("test?.txt", invalidCharacter: "?", os: .windows)
+  testPath("<test.txt", invalidCharacter: "<", os: .windows)
+  testPath("test>.txt", invalidCharacter: ">", os: .windows)
+  testPath("test:.txt", invalidCharacter: ":", os: .windows)
+  testPath("test|", invalidCharacter: "|", os: .windows)
+  testPath("te\"st.txt", invalidCharacter: "\"", os: .windows)
 }
--- Tests/filenames-scanTests/filenames_scanTests.swift
+++ Tests/filenames-scanTests/filenames_scanTests.swift
@@ -1,13 +1,7 @@
-import XCTest
+import Testing
 
 @testable import filenames_scan
 
-final class filenames_scanTests: XCTestCase {
-  func testExample() throws {
-    // XCTest Documenation
-    // https://developer.apple.com/documentation/xctest
-
-    // Defining Test Cases and Test Methods
-    // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-  }
+@Test func initializable() throws {
+  let _ = FilenamesScan()
 }
