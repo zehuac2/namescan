0b1b1fa5bbd9418a9d395daaf5c3c9cfabf801a1
Zehua Chen <31496190+zehuac2@users.noreply.github.com>
2025-06-29 23:25:24 -0700

    [test] Create test for linux

--- Tests/FilenamesScanTests/FilenameScannerTests.swift
+++ Tests/FilenamesScanTests/FilenameScannerTests.swift
@@ -9,20 +9,27 @@ import Testing
 
 @testable import FilenamesScan
 
-func testPath(_ path: String, invalidCharacter: Character, os: FilenameScannerResult.OS) {
-  let url = URL(filePath: path)
-  let filenameScanner = FilenameScanner()
-  let results = filenameScanner.scan(url)
+@Suite("FilenameScannerTests")
+struct FilenameScannerTests {
+  private func testPath(_ path: String, invalidCharacter: Character, os: FilenameScannerResult.OS) {
+    let url = URL(filePath: path)
+    let filenameScanner = FilenameScanner()
+    let results = filenameScanner.scan(url)
 
-  #expect(results == [.invalid(url: url, character: invalidCharacter, os: os)])
-}
+    #expect(results == [.invalid(url: url, character: invalidCharacter, os: os)])
+  }
 
-@Test func windows() {
-  testPath("\\test.txt", invalidCharacter: "\\", os: .windows)
-  testPath("test?.txt", invalidCharacter: "?", os: .windows)
-  testPath("<test.txt", invalidCharacter: "<", os: .windows)
-  testPath("test>.txt", invalidCharacter: ">", os: .windows)
-  testPath("test:.txt", invalidCharacter: ":", os: .windows)
-  testPath("test|", invalidCharacter: "|", os: .windows)
-  testPath("te\"st.txt", invalidCharacter: "\"", os: .windows)
+  @Test func windows() async throws {
+    testPath("\\test.txt", invalidCharacter: "\\", os: .windows)
+    testPath("test?.txt", invalidCharacter: "?", os: .windows)
+    testPath("<test.txt", invalidCharacter: "<", os: .windows)
+    testPath("test>.txt", invalidCharacter: ">", os: .windows)
+    testPath("test:.txt", invalidCharacter: ":", os: .windows)
+    testPath("test|", invalidCharacter: "|", os: .windows)
+    testPath("te\"st.txt", invalidCharacter: "\"", os: .windows)
+  }
+
+  @Test func linux() async throws {
+    testPath("test/test.txt", invalidCharacter: "/", os: .linux)
+  }
 }
