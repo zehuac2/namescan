b6577cff75328ecc33d18b24dc9bb74f657d8b3a
Zehua Chen <31496190+zehuac2@users.noreply.github.com>
2025-06-29 22:39:43 -0700

    [chore] Bump platform version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "FilenamesScan",
-  platforms: [.macOS("13.0")],
+  platforms: [.macOS("15.0")],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .executable(
