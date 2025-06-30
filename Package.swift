fc3459c160c925f3adcad42c6463ec9813206393
Zehua Chen <31496190+zehuac2@users.noreply.github.com>
2025-06-29 22:33:12 -0700

    [chore] Upgrade swift arg parser.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
       targets: ["filenames-scan"])
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0")
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.1")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
